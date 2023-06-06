1de5d6f7837ac2b4d88ba0c9ed7868584e547db4
cdga777 <cdga777@gmail.com>
2023-06-06 07:04:43 -0500

    cleaning code, log settings

--- scutinativesdk/src/main/java/com/mindtrust/scutinativesdk/ScutiWebView.kt
+++ scutinativesdk/src/main/java/com/mindtrust/scutinativesdk/ScutiWebView.kt
@@ -23,8 +23,12 @@ class ScutiWebView : Fragment()  {
     private var base_url = "https://dev.run.app.scuti.store/?gameId=1e6e003f-0b94-4671-bc35-ccc1b48ce87d&platform=Unity"
     private lateinit var webView:WebView
 
-    private lateinit var targetEnvironment:TargetEnvironment;
-    private lateinit var appId:String;
+    private lateinit var targetEnvironment:TargetEnvironment
+    private lateinit var _logSettings: LogSettings
+    var logSettings: LogSettings = _logSettings
+        get() = _logSettings
+
+    private lateinit var appId:String
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -36,22 +40,20 @@ class ScutiWebView : Fragment()  {
 
         webView  = view.findViewById(R.id.myWebView)
 
-        Log.d("INFO", "<*****<<--0 onCreateView 0-->>*****>");
-        Log.d("INFO", "context::"+context);
-
         if (context is ScutiInterface) {
-            Log.d("INFO", "ScutiInterface::"+context);
             callback = context as ScutiInterface
         }
 
         webView.webViewClient = object : WebViewClient(){
             @RequiresApi(Build.VERSION_CODES.M)
             override fun onPageFinished(view: WebView, url: String) {
-                Log.d("INFO", "<******0 Page Loaded 0******> "+url);
 
                 if (url.startsWith("unity:")) {
-                    val message = url.substring(6);
-                    Log.d("INFO", "<******0 unity: 0******> "+message);
+                    if(logSettings.ordinal >= LogSettings.VERBOSE.ordinal)
+                    {
+                        val message = url.substring(6);
+                        Log.d("INFO", " unity: "+message);
+                    }
 
                 } else {
 
@@ -73,101 +75,72 @@ class ScutiWebView : Fragment()  {
                                 "         }\n" +
                                 "     }\n" +
                                 " }", null
-                    );
+                    )
                     //webView.evaluateJavascript("initializeApp();", null);
                     getNewProducts()
                     getNewRewards()
 
                 }
-                /*val getNewProductsMessage = WebMessage("getNewProducts();");
-                webView.postWebMessage(getNewProductsMessage, Uri.parse(url));
-                val getNewRewardsMessage = WebMessage("getNewRewards();");
-                webView.postWebMessage(getNewRewardsMessage, Uri.parse(url));*/
-                /*if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.KITKAT) {
-                    webView.evaluateJavascript("getNewProducts();", null/*onJsEvalCallback()*/);
-                } else {
-                    webView.loadUrl("javascript:getNewProducts();");
-                }*/
+
             }
         }
-        webView.settings.domStorageEnabled = true;
+        webView.settings.domStorageEnabled = true
         //webView.settings.databaseEnabled = true;
 
         webView.settings.javaScriptEnabled = true
         //webView.settings.useWideViewPort = true
 
-        webView.addJavascriptInterface(JSBridge(requireContext(), this),"JSBridge");
-
-        Log.d("INFO", "onCreate ScutiWebView callback::"+callback);
+        webView.addJavascriptInterface(JSBridge(requireContext(), this),"JSBridge")
 
         callback?.onWebViewLoadCompleted()
 
-        return view;
+        return view
     }
 
-    fun  init(environment:TargetEnvironment, id:String) {
-        targetEnvironment = environment;
-        appId = id;
-
+    fun  init(environment:TargetEnvironment, id:String, logSettings: LogSettings) {
+        targetEnvironment = environment
+        _logSettings = logSettings
+        appId = id
         base_url = targetEnvironment.type+"?gameId="+appId+"&platform=Unity"
-        /*val userToken = getToken();
-        Log.d("INFO", "<----0 INIT ScutiWebView 0----> TOKEN:{"+userToken+"}");
-        base_url = if (!userToken.isNullOrBlank()) {
-            targetEnvironment.type+"?gameId="+appId+"&platform=Unity&userToken="+userToken
-        } else {
-            targetEnvironment.type+"?gameId="+appId+"&platform=Unity"
-        }*/
     }
 
     fun load(){
-        Log.d("INFO", "<----0 LOAD ScutiWebView 0----> ");
-        val userToken = getToken();
-        Log.d("INFO", "<----8 INIT ScutiWebView 8----> TOKEN:{"+userToken+"}");
+        val userToken = getToken()
         base_url = if (userToken.isNullOrBlank()) {
             targetEnvironment.type+"?gameId="+appId+"&platform=Unity&userToken="+userToken
         } else {
             targetEnvironment.type+"?gameId="+appId+"&platform=Unity"
         }
-        Log.d("INFO", "loadUrl: "+base_url);
         webView.loadUrl(base_url)
     }
 
     fun getNewProducts() {
-        Log.d("INFO", "<----0 getNewProducts() 0----> ");
         webView.evaluateJavascript("getNewProducts();", null)
     }
 
     fun getNewRewards() {
-        Log.d("INFO", "<----0 getNewRewards() 0----> ");
         webView.evaluateJavascript("getNewRewards();", null)
     }
 
     fun setUserId(userId:String) {
-        Log.d("INFO", "<----0 setGameUserId(\""+userId+"\") 0----> ");
         webView.evaluateJavascript("setGameUserId(\""+userId+"\");", null)
     }
 
     internal fun saveToken(token:String){
-        Log.d("INFO", "<----0 Saving User Token {"+token+"} 0----> ");
         val sharedPref = activity?.getPreferences(Context.MODE_PRIVATE) ?: return
         with (sharedPref.edit()) {
-            Log.d("INFO", "   <----0 Saving... {"+token+"} 0----> ");
             putString(getString(R.string.user_token), token)
             apply()
         }
     }
 
     internal fun getToken(): String? {
-        Log.d("INFO", "<----0 Getting User Token 0----> ");
         val sharedPref = activity?.getPreferences(Context.MODE_PRIVATE) ?: return ""
-        Log.d("INFO", "   <----0 Getting... 0----> ");
-        return sharedPref.getString(getString(R.string.user_token), "");
+        return sharedPref.getString(getString(R.string.user_token), "")
     }
 
     internal fun clearToken() {
-        Log.d("INFO", "<----0 Clearing User Token 0----> ");
         val sharedPref = activity?.getPreferences(Context.MODE_PRIVATE) ?: return
-        Log.d("INFO", "<----0 Clearing... 0----> ");
         with (sharedPref.edit()) {
             remove(getString(R.string.user_token))
             commit()
@@ -180,20 +153,25 @@ class ScutiWebView : Fragment()  {
     class JSBridge(val context: Context, val view:ScutiWebView){
         @JavascriptInterface
         fun showMessageInNative(message:String){
-            Log.d("INFO", "<******0== Message ==0******> "+message);
             val callback = context as ScutiInterface
             val answer = JSONObject(message)
-            Log.d("INFO", "<******0== Message Value ==0******> "+answer.get("message"));
-            Toast.makeText(context,message, Toast.LENGTH_LONG).show()
+            //Toast.makeText(context,message, Toast.LENGTH_LONG).show()
+            if(view.logSettings.ordinal >= LogSettings.VERBOSE.ordinal)
+            {
+                println(message)
+            }
             when(answer.get("message") as String){
-                ScutiStoreMessages.MSG_BACK_TO_THE_GAME.type -> callback?.onBackToTheGame()
-                ScutiStoreMessages.MSG_SCUTI_EXCHANGE.type -> callback?.onScutiExchange((answer.get("payload") as Int).toString())
-                ScutiStoreMessages.MSG_NEW_REWARDS.type -> callback?.onNewRewards(answer.get("payload") as Int > 0)
-                ScutiStoreMessages.MSG_NEW_PRODUCTS.type -> callback?.onNewProducts(answer.get("payload") as Int > 0)
+                ScutiStoreMessages.MSG_BACK_TO_THE_GAME.type -> callback.onBackToTheGame()
+                ScutiStoreMessages.MSG_SCUTI_EXCHANGE.type -> callback.onScutiExchange((answer.get("payload") as Int).toString())
+                ScutiStoreMessages.MSG_NEW_REWARDS.type -> callback.onNewRewards(answer.get("payload") as Int > 0)
+                ScutiStoreMessages.MSG_NEW_PRODUCTS.type -> callback.onNewProducts(answer.get("payload") as Int > 0)
                 ScutiStoreMessages.MSG_USER_TOKEN.type -> view.saveToken(answer.get("payload") as String)
-                ScutiStoreMessages.MSG_STORE_IS_READY.type -> callback?.onStoreIsReady()
+                ScutiStoreMessages.MSG_STORE_IS_READY.type -> callback.onStoreIsReady()
                 ScutiStoreMessages.MSG_LOG_OUT.type -> view.clearToken()
-                else -> println(message)
+                else -> if(view.logSettings.ordinal >= LogSettings.VERBOSE.ordinal)
+                {
+                    println(message)
+                }
             }
         }
     }
